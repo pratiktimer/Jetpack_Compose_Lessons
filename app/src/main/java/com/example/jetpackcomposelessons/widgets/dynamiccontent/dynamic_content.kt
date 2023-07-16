b5432c9c96abd57e6c8d2a90a1355f6642dcf6f1
praban <pbanawalkar2@gmail.com>
2023-07-16 21:00:33 +0530

    lifting state of textfield lesson completed

--- app/src/main/java/com/example/jetpackcomposelessons/widgets/dynamiccontent/dynamic_content.kt
+++ app/src/main/java/com/example/jetpackcomposelessons/widgets/dynamiccontent/dynamic_content.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,22 +22,35 @@ val namesList: ArrayList<String> = arrayListOf("pratik","andrea","diana","nikhil
 @Composable
 fun MainScreen(){
     val greetingListState = remember { mutableStateListOf<String>(namesList[0]) }
-
+    val textFieldState = remember { mutableStateOf("") }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        GreetingList(greetingListState) { greetingListState.add("Sharvari") }
+        GreetingList(
+            greetingListState,
+            { greetingListState.add(textFieldState.value) },
+            textFieldState.value,
+            { newName -> textFieldState.value = newName }
+        )
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingList(namesList: List<String>, onClick: () -> Unit){
+fun GreetingList(
+    namesList: List<String>,
+    onClick: () -> Unit,
+    newTextFieldValue: String,
+    UpdateTextFieldValue: (newName: String) -> Unit
+
+){
 
     for (name in namesList){
         Greeting(name = name)
     }
+    TextField(value = newTextFieldValue, onValueChange = UpdateTextFieldValue)
     Button(
         modifier = Modifier.wrapContentSize(align = Alignment.Center),
         onClick = onClick
